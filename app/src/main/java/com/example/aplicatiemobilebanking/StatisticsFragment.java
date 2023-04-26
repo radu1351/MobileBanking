a0dc3b382ee89fce68e1bc68e2e8fe1b1c7c638b
radu1351 <radu1351@yahoo.com>
2023-04-26 22:54:16 +0300

    26 Apr

--- app/src/main/java/com/example/aplicatiemobilebanking/StatisticsFragment.java
+++ app/src/main/java/com/example/aplicatiemobilebanking/StatisticsFragment.java
@@ -1,47 +1,35 @@
 package com.example.aplicatiemobilebanking;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatisticsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.androidplot.pie.PieChart;
+import com.androidplot.pie.PieRenderer;
+import com.androidplot.pie.Segment;
+import com.androidplot.pie.SegmentFormatter;
+import com.androidplot.ui.DynamicTableModel;
+import com.androidplot.util.PixelUtils;
+
+
 public class StatisticsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    PieChart pieChartTransactions;
 
     public StatisticsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatisticsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static StatisticsFragment newInstance(String param1, String param2) {
         StatisticsFragment fragment = new StatisticsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +38,51 @@ public class StatisticsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+        pieChartTransactions = view.findViewById(R.id.statFrag_pcTransactions);
+
+        createPieChart();
+
+        return view;
+    }
+
+    private void createPieChart() {
+        Segment segmentGroceries = new Segment("Groceries", 20);
+        Segment segmentShopping = new Segment("Shopping", 20);
+        Segment segmentTransport = new Segment("Transport", 20);
+        Segment segmentRestaurant = new Segment("Restaurant", 10);
+        Segment segmentUtilities = new Segment("Utilities", 10);
+        Segment segmentOthers = new Segment("Others", 10);
+
+        SegmentFormatter segmentFormatterGroceries = new SegmentFormatter(Color.rgb(0, 15, 75));
+        SegmentFormatter segmentFormatterShopping = new SegmentFormatter(Color.rgb(15, 35, 95));
+        SegmentFormatter segmentFormatterTransport = new SegmentFormatter(Color.rgb(30, 65, 125));
+        SegmentFormatter segmentFormatterRestaurant = new SegmentFormatter(Color.rgb(50, 90, 150));
+        SegmentFormatter segmentFormatterUtilities = new SegmentFormatter(Color.rgb(70, 115, 175));
+        SegmentFormatter segmentFormatterOthers = new SegmentFormatter(Color.rgb(90, 140, 200));
+
+
+
+        segmentFormatterGroceries.getLabelPaint().setTextSize(35f);
+        segmentFormatterShopping.getLabelPaint().setTextSize(35f);
+        segmentFormatterTransport.getLabelPaint().setTextSize(35f);
+        segmentFormatterRestaurant.getLabelPaint().setTextSize(35f);
+        segmentFormatterUtilities.getLabelPaint().setTextSize(35f);
+        segmentFormatterOthers.getLabelPaint().setTextSize(35f);
+
+        pieChartTransactions.addSegment(segmentGroceries, segmentFormatterGroceries);
+        pieChartTransactions.addSegment(segmentShopping, segmentFormatterShopping);
+        pieChartTransactions.addSegment(segmentTransport, segmentFormatterTransport);
+        pieChartTransactions.addSegment(segmentRestaurant, segmentFormatterRestaurant);
+        pieChartTransactions.addSegment(segmentUtilities, segmentFormatterUtilities);
+        pieChartTransactions.addSegment(segmentOthers, segmentFormatterOthers);
     }
 }
